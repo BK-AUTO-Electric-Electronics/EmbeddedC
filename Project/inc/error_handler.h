5f322c3694bcc412d359e4090314516506f092a3
phamdinhminh20 <minhphamdinh63@gmail.com>
2025-12-16 14:40:13 +0700

    Delete Project directory

--- Project/inc/error_handler.h
+++
@@ -1,6 +0,0 @@
-#ifndef ERROR_HANDLER_H
-#define ERROR_HANDLER_H
-
-void handle_divide_error(float mau);
-
-#endif
--- Project/inc/math_utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef MATH_UTILS_H
-#define MATH_UTILS_H
-
-double divide(int tong_tu, int tong_mau,...);
-
-#endif
