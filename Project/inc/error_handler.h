abe0c69ac867beaf004c5c9f9fa4285a8786faed
phamdinhminh20 <minhphamdinh63@gmail.com>
2025-12-16 14:06:10 +0700

    Project

---
+++ Project/inc/error_handler.h
@@ -0,0 +1,6 @@
+#ifndef ERROR_HANDLER_H
+#define ERROR_HANDLER_H
+
+void handle_divide_error(float mau);
+
+#endif
---
+++ Project/inc/math_utils.h
@@ -0,0 +1,6 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+double divide(int tong_tu, int tong_mau,...);
+
+#endif
