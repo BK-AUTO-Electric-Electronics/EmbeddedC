8c03b1cf8531899a6325ea6cc12221b73686b4e5
phamdinhminh20 <minhphamdinh63@gmail.com>
2025-12-16 14:35:41 +0700

    Delete Project directory

--- Project/src/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "math_utils.h"
-#include "error_handler.h"
-
-int main() {
-    double result = divide(4, 1, 1, 2, 3, 4, 1);
-    printf ("Result = %f\n", result);
-    return 0;
-}
